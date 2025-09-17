f33345b50e513dad849f1cf83175078214ebeb99
cgruver <cgruver@redhat.com>
2025-09-17 13:22:35 +0000

    refactor

--- workspace-backup-image/entrypoint.sh
+++ workspace-backup-image/entrypoint.sh
@@ -3,11 +3,5 @@
 set -x
 set -e
 
-BACKUP_IMAGE=${DEVWORKSPACE_BACKUP_REGISTRY}/backup-${DEVWORKSPACE_NAMESPACE}-${DEVWORKSPACE_NAME}:latest
-NEW_IMAGE=$(buildah from scratch)
-buildah copy ${NEW_IMAGE} /workspace-pvc/ /
-buildah config --label DEVWORKSPACE=${DEVWORKSPACE_NAME} ${NEW_IMAGE}
-buildah config --label NAMESPACE=${DEVWORKSPACE_NAMESPACE} ${NEW_IMAGE}
-buildah commit ${NEW_IMAGE} ${BACKUP_IMAGE}
-buildah umount ${NEW_IMAGE}
-buildah push ${BACKUP_IMAGE}
+exec "$@"
+
---
+++ workspace-backup-image/workspace-recovery.sh
@@ -0,0 +1,30 @@
+function backup() {
+  BACKUP_IMAGE=${DEVWORKSPACE_BACKUP_REGISTRY}/backup-${DEVWORKSPACE_NAMESPACE}-${DEVWORKSPACE_NAME}:latest
+  NEW_IMAGE=$(buildah from scratch)
+  buildah copy ${NEW_IMAGE} /workspace-pvc/ /
+  buildah config --label DEVWORKSPACE=${DEVWORKSPACE_NAME} ${NEW_IMAGE}
+  buildah config --label NAMESPACE=${DEVWORKSPACE_NAMESPACE} ${NEW_IMAGE}
+  buildah commit ${NEW_IMAGE} ${BACKUP_IMAGE}
+  buildah umount ${NEW_IMAGE}
+  buildah push ${BACKUP_IMAGE}
+}
+
+function restore() {
+  BACKUP_IMAGE=${WORKSPACE_REGISTRY}/backup-${DEVWORKSPACE_NAMESPACE}-${DEVWORKSPACE_NAME}:latest
+  podman create --name workspace-restore ${BACKUP_IMAGE}
+  rm -rf ${PROJECTS_ROOT}
+  mkdir -p ${PROJECTS_ROOT}
+  podman cp workspace-restore:/ ${PROJECTS_ROOT}
+}
+
+for i in "$@"
+do
+  case $i in
+    --backup)
+      backup
+    ;;
+    --restore)
+      restore
+    ;;
+  esac
+done
